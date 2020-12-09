f66903cdd9e8752496322e09587e615f5b784674
Elia Chiarucci <eliachiarucci@MacBook-Pro-di-Elia.local>
2020-12-09 01:11:58 +0100

    changed production url

--- src/env.js
+++ src/env.js
@@ -5,7 +5,7 @@ const dev = {
 
 const prod = {
   URL: "prod",
-  SERVER_URL: "http://localhost:5000"
+  SERVER_URL: "https://mr-pan-backend-v6k9j.ondigitalocean.app/"
 };
 
 const env = process.env.NODE_ENV === "production" ? prod : dev;
